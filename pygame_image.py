cfd029e7eeaa125f5e6bd7af7f157cf6324816fc
Akichika <c0a24204d1@edu.teu.ac.jp>
2025-06-10 16:00:00 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -18,13 +18,12 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img,[-tmr,0])
         
-
         
-        screen.blit(bg_img, [0, 0]) #0,0に貼り付ける　ループ文の中でブリッドしないと表示されない
         screen.blit(bg_img,[-tmr,0])
+        x = tmr
+        screen.blit(bg_img,[-x,0]) #1枚目
+        screen.blit(bg_img,[-x+1600, 0])#2枚目
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
